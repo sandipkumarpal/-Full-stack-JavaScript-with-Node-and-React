7307d47cef3ada308b9d20bb1e21b97209fddd91
Sandip Pal <sandip.pal18@gmail.com>
2018-02-24 20:52:10 +0530

    Added Index.html and Used Express Server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer();
 
-server.on('request', (req, res) => {
-    res.write("hello World");
-    setTimeout(() => {
-        res.write('Still on.....');
-        res.end(); 
-    });
-    
-});
+const express = require('express');
 
-server.listen(9000);
+// server.on('request', (req, res) => {
+//     // res.write("hello World");
+//     // setTimeout(() => {
+//     //     res.write('Still on.....');
+//     //     res.end(); 
+//     // });
+//     res.end(fs.readFileSync(__dirname + '/../client/index.html'));
+// });
+
+// server.listen(9000);
+
+const app = express();
+app.use(express.static('client'));
+
+
+app.listen(9000);
+
+
